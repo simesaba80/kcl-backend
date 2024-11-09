907204141c42c99c6296defad08d5dbf1a5bb72c
simesaba80 <simesaba1120@gmail.com>
2024-11-09 22:46:27 +0900

    モデルの更新

--- model/user.go
+++ model/user.go
@@ -6,23 +6,21 @@ import (
 
 // User is a struct that represents a user.
 type User struct {
-	ID         int       `json:"id"`
-	Name       string    `json:"name"`
-	Sex        string    `json:"sex"`
-	Email      string    `json:"email"`
-	Height     int       `json:"height"`
-	Age        int       `json:"age"`
-	Job        string    `json:"job"`
-	FitbitID   int       `json:"fitbit_id"`
-	MomentumID int       `json:"momentum_id"`
-	SleepID    int       `json:"sleep_id"`
-	MealID     int       `json:"meal_id"`
-	CreatedAt  time.Time `json:"created_at"`
+	ID        int       `json:"id"`
+	UID       string    `json:"uid"`
+	Name      string    `json:"name"`
+	Sex       string    `json:"sex"`
+	Email     string    `json:"email"`
+	Height    int       `json:"height"`
+	Age       int       `json:"age"`
+	Job       string    `json:"job"`
+	CreatedAt time.Time `json:"created_at"`
 }
 
 type Momentum struct {
 	// Momentum is a struct that represents a user's Momentum data.
-	FitbitID     int    `json:"fitbit_id"`
+	ID           int    `json:"id"`
+	UserID       int    `json:"user_id"`
 	Steps        int    `json:"steps"`
 	Calories     int    `json:"calories"`
 	Distance     int    `json:"distance"`
@@ -33,7 +31,8 @@ type Momentum struct {
 
 type Sleep struct {
 	// Sleep is a struct that represents a user's sleep data.
-	FitbitID   int       `json:"fitbit_id"`
+	ID         int       `json:"id"`
+	UserID     int       `json:"user_id"`
 	Hours      int       `json:"hours"`
 	StartedAt  time.Time `json:"started_at"`
 	EndedAt    time.Time `json:"ended_at"`
@@ -46,7 +45,8 @@ type Sleep struct {
 
 type Meal struct {
 	// Meal is a struct that represents a user's meal data.
-	FitbitID       int       `json:"fitbit_id"`
+	ID             int       `json:"id"`
+	UserID         int       `json:"user_id"`
 	MealName       string    `json:"meal_name"`
 	CaloriePer100G int       `json:"calories_per_100g"`
 	Date           time.Time `json:"date"`

4372e800c951e808ad285c1499aee2e91f338d1a
simesaba80 <simesaba1120@gmail.com>
2024-11-11 16:15:57 +0900

    bunのタグの追加

--- database/model.go
+++ database/model.go
@@ -6,48 +6,47 @@ import (
 
 // User is a struct that represents a user.
 type User struct {
-	ID        int `bun:"id,pk,autoincrement"`
-	UID       string
-	Name      string
-	Sex       string
-	Email     string
-	Height    int
-	Age       int
-	Job       string
-	CreatedAt time.Time
+	ID        int       `bun:"id,pk,autoincrement"`
+	UID       string    `bun:"uid,unique,notnull"`
+	Name      string    `bun:"name,notnull"`
+	Sex       string    `bun:"sex,notnull"`
+	Height    int       `bun:"height,nullzero"`
+	Age       int       `bun:"age,nullzero"`
+	Job       string    `bun:"job,nullzero"`
+	CreatedAt time.Time `bun:"created_at"`
 }
 
 type Momentum struct {
 	// Momentum is a struct that represents a user's Momentum data.
-	ID           int    `json:"id" bun:"id, pk, autoincrement"`
-	UserID       int    `json:"user_id"`
-	Steps        int    `json:"steps"`
-	Calories     int    `json:"calories"`
-	Distance     int    `json:"distance"`
-	MaxHeartRate int    `json:"max_heart_rate"`
-	MinHeartRate int    `json:"min_heart_rate"`
-	Date         string `json:"date"`
+	ID           int    `bun:"id,pk,autoincrement"`
+	UserID       int    `bun:"user_id,notnull"`
+	Steps        int    `bun:"steps,notnull"`
+	Calories     int    `bun:"calories,notnull"`
+	Distance     int    `bun:"distance,notnull"`
+	MaxHeartRate int    `bun:"max_heart_rate,notnull"`
+	MinHeartRate int    `bun:"min_heart_rate,notnull"`
+	Date         string `bun:"date,notnull"`
 }
 
 type Sleep struct {
 	// Sleep is a struct that represents a user's sleep data.
-	ID         int       `json:"id" bun:"id, pk, autoincrement"`
-	UserID     int       `json:"user_id"`
-	Hours      int       `json:"hours"`
-	StartedAt  time.Time `json:"started_at"`
-	EndedAt    time.Time `json:"ended_at"`
-	DeepSleep  int       `json:"deep_sleep"`
-	LightSleep int       `json:"light_sleep"`
-	RemSleep   int       `json:"rem_sleep"`
-	Wake       int       `json:"wake"`
-	Date       string    `json:"date"`
+	ID         int       `bun:"id, pk, autoincrement"`
+	UserID     int       `bun:"user_id,notnull"`
+	Hours      int       `bun:"hours,notnull"`
+	StartedAt  time.Time `bun:"started_at,notnull"`
+	EndedAt    time.Time `bun:"ended_at,notnull"`
+	DeepSleep  int       `bun:"deep_sleep,notnull"`
+	LightSleep int       `bun:"light_sleep,notnull"`
+	RemSleep   int       `bun:"rem_sleep,notnull"`
+	Wake       int       `bun:"wake,notnull"`
+	Date       string    `bun:"date,notnull"`
 }
 
 type Meal struct {
 	// Meal is a struct that represents a user's meal data.
-	ID             int       `json:"id" bun:"id, pk, autoincrement"`
-	UserID         int       `json:"user_id"`
-	MealName       string    `json:"meal_name"`
-	CaloriePer100G int       `json:"calories_per_100g"`
-	Date           time.Time `json:"date"`
+	ID             int    `bun:"id, pk, autoincrement"`
+	UserID         int    `bun:"user_id,notnull"`
+	MealName       string `bun:"meal_name,notnull"`
+	CaloriePer100G int    `bun:"calorie_per_100g,notnull"`
+	Date           time.Time
 }

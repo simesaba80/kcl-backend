6b8ca603e48d29638b424ee6e1bef39ea82582c6
simesaba80 <simesaba1120@gmail.com>
2024-11-14 00:35:24 +0900

    ミドルウェアの追加

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 	"github.com/simesaba80/kcl-back/db"
 	"github.com/simesaba80/kcl-back/services"
 	"github.com/simesaba80/kcl-back/utils"
@@ -11,6 +12,11 @@ func main() {
 	e := echo.New()
 	utils.LoadConfig()
 	db.Connectdb()
+	e.Use(middleware.LoggerWithConfig(middleware.LoggerConfig{
+		Format: "time=${time_rfc3339_nano}, method=${method}, uri=${uri}, status=${status}\n",
+	}))
+	e.Use(middleware.CORS())
+	e.Use(middleware.Recover())
 
 	e.GET("/", services.Hello)
 	e.GET("/user", services.GetUsers)
@@ -19,6 +25,7 @@ func main() {
 	e.PUT("/user/update/:id", services.UpdateUser)
 	e.GET("/momentum/:id", services.GetUserMomentumData)
 	e.GET("/sleep/:id", services.GetUserSleepData)
+	e.GET("/meal/:id", services.GetUserMealData)
 
 	e.Start(":8080")
 }
